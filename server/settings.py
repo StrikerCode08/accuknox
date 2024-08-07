e3f7475275ff35191556c57d416823760990642d
StrikerCode08 <akashbajpai227@gmail.com>
2024-08-07 10:46:12 +0530

    db url change

--- server/settings.py
+++ server/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'neondb',
-        'USER': 'neondb_owner',
-        'PASSWORD': 'l2gMLduVH7wJ',
-        'HOST': 'ep-white-dew-a51pv7d3.us-east-2.aws.neon.tech',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
